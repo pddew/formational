a3f794cfde210cf80414a07b339e45396fe21768
Patrick Dewhurst <paddy@gopro-2205.lan>
2018-11-27 15:40:10 +0000

    title and description added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Formational - Product and Design Agency",
+    author: "Paddy Dewhurst",
+    description: "Formational is a product and design company in London, specialising in sport and healthcare web production"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
