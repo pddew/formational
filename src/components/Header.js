1ca9d56b4aeee0879b82182231a4740b15b9d128
Patrick Dewhurst <paddy@gopro-2205.lan>
2018-11-27 15:53:10 +0000

    Updated introduction pages.

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
-            </div>
+                <h1>Formational Design</h1>
+                <p>Formational is a digital design agency based in London, specialising in websites for the sports and healthcare sectors.</p>           </div>
         </div>
         <nav>
             <ul>
