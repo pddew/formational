a11ffb35e7104613efcafcc911146af2fb241223
pddew <paddydewhurst@gmail.com>
2019-03-27 15:03:33 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Outro</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
